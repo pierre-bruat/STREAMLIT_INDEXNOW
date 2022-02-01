54cb880ded0e612ecde04e4c11daf8f1386e70e6
Pierre Bruat <pbruat33@gmail.com>
2022-02-01 14:29:09 +0100

    Update indexnow_streamlit.py

--- indexnow_streamlit.py
+++ indexnow_streamlit.py
@@ -9,10 +9,11 @@ import streamlit as st
 
 ###### FORMULAIRE ########
 header = st.title('Index my urls now')
-form = st.form(key='my-form')
-api_key = form.text_input("Insert your API key")
-urls_list = form.file_uploader("Choose a CSV file", accept_multiple_files=True)
-submit_1 = form.form_submit_button('Submit')
+
+form_1 = st.form(key='my-form')
+api_key = form_1.text_input("Insert your API key")
+urls_list = form_1.file_uploader("Choose a CSV file", accept_multiple_files=True)
+submit_1 = form_1.form_submit_button('Submit')
 if submit_1:
 	for i in urls_list:
 		endpoint= f"https://bing.com/indexnow?url={i}&key={api_key}"
@@ -23,8 +24,10 @@ if submit_1:
 		else: st.write(f"❌ something went wrong with {i}")
 		time.sleep(2)
 
-xml_sitemap = form.text_input("Insert your XML sitemap url")
-submit_2 = form.form_submit_button('Submit')
+form_2 = st.form(key='my-form')
+api_key = form_2.text_input("Insert your API key")
+xml_sitemap = form_2.text_input("Insert your XML sitemap url")
+submit_2 = form_2.form_submit_button('Submit')
 if submit_2:
 	sitemap_urls = adv.sitemap_to_df(xml_sitemap)
 	urls = sitemap_urls["loc"].to_list()
