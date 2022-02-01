9a214f63d26e4d2b3aed3836297198f585f31415
Pierre Bruat <pbruat33@gmail.com>
2022-02-01 18:24:59 +0100

    Update indexnow_streamlit.py

--- indexnow_streamlit.py
+++ indexnow_streamlit.py
@@ -12,22 +12,24 @@ from urllib import request
 
 ###### FORMULAIRE ########
 
+def input_to_df(input):
+		if uploaded_file is not None:
+		dataframe = pd.read_csv(uploaded_file)
+
+
 
 header = st.title('Index my urls now')
+form = st.form(key='my-form')
 
-api_key = text_input("Insert your API key")
-xml_sitemap = text_input("Insert your XML sitemap url")
-uploaded_file = st.file_uploader("Upload your keywords")
-
-
-submit = form_submit_button('Submit')
+api_key = form.text_input("Insert your API key")
+xml_sitemap = form.text_input("Insert your XML sitemap url")
+uploaded_file = form.file_uploader("Upload your CSV file")
+submit = form.form_submit_button('Submit')
 
 if submit:
-	sitemap_urls = adv.sitemap_to_df(xml_sitemap)
-	urls = sitemap_urls["loc"].to_list()
-	if uploaded_file is not None:
-		dataframe = pd.read_csv(uploaded_file)
-		st.write(dataframe)
+	#sitemap_urls = adv.sitemap_to_df(xml_sitemap)
+	dataframe = input_to_df(uploaded_file)
+	st.write(dataframe)
 	#for i in urls:
 		#endpoint= f"https://bing.com/indexnow?url={i}&key={api_key}"
 		#response = requests.get(endpoint)
