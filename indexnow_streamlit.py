284f5041b0e6f1cb0bae901af5358fb7deb4d889
Pierre Bruat <pbruat33@gmail.com>
2022-02-01 14:06:08 +0100

    Update indexnow_streamlit.py

--- indexnow_streamlit.py
+++ indexnow_streamlit.py
@@ -20,8 +20,8 @@ if submit:
 	for i in urls:
 		endpoint= f"https://bing.com/indexnow?url={i}&key={api_key}"
 		response = requests.get(endpoint)
-		#if response.content == "[200]":
-			#st.write(f"Job is done for {i}")
-		#else: st.write(f"something went wrong with {i}")
-		st.write(response)
+		if response == "<Response [200]>":
+			st.write(f"Job is done for {i}")
+		else: st.write(f"something went wrong with {i}")
+		#st.write(response)
 		time.sleep(2)
