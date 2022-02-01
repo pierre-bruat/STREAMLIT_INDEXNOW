5a099b9612acc387f91e6dcbd8950898c1f4fcfd
Pierre Bruat <pbruat33@gmail.com>
2022-02-01 17:45:20 +0100

    Update indexnow_streamlit.py

--- indexnow_streamlit.py
+++ indexnow_streamlit.py
@@ -13,7 +13,7 @@ header = st.title('Index my urls now')
 form = st.form(key='my-form')
 api_key = form.text_input("Insert your API key")
 #xml_sitemap = form.text_input("Insert your XML sitemap url")
-urls_list = form.file_uploader("Choose a CSV file", accept_multiple_files=True, type=["csv"])
+urls_list = st.file_uploader("Choose a CSV file", accept_multiple_files=True, type=["csv"])
 submit = form.form_submit_button('Submit')
 if submit: 
 	dataframe = pd.read_csv(urls_list)
