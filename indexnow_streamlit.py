09b7eddd23710442380a381855d15aac05c0ab65
Pierre Bruat <pbruat33@gmail.com>
2022-02-01 14:37:00 +0100

    Update indexnow_streamlit.py

--- indexnow_streamlit.py
+++ indexnow_streamlit.py
@@ -13,20 +13,18 @@ header = st.title('Index my urls now')
 form = st.form(key='my-form')
 api_key = form.text_input("Insert your API key")
 urls_list = form.file_uploader("Choose a CSV file", accept_multiple_files=True)
-submit = form.form_submit_button('Submit')
-if submit:
-	for i in urls_list:
-		endpoint= f"https://bing.com/indexnow?url={i}&key={api_key}"
-		response = requests.get(endpoint)
-		st.write(response)
-		if response != "error":
-			st.write(f"✅ URL submitted successfully for {i}")
-		else: st.write(f"❌ something went wrong with {i}")
-		time.sleep(2)
-
-form_2 = st.form(key='my-form-2')
 xml_sitemap = form_.text_input("Insert your XML sitemap url")
-submit = form.form_2_submit_button('Submit')
+submit = form.form_submit_button('Submit')
+#if submit:
+	#for i in urls_list:
+		#endpoint= f"https://bing.com/indexnow?url={i}&key={api_key}"
+		#response = requests.get(endpoint)
+		#st.write(response)
+		#if response != "error":
+			#st.write(f"✅ URL submitted successfully for {i}")
+		#else: st.write(f"❌ something went wrong with {i}")
+		#time.sleep(2)
+
 if submit:
 	sitemap_urls = adv.sitemap_to_df(xml_sitemap)
 	urls = sitemap_urls["loc"].to_list()
