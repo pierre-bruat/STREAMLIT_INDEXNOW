5280c0ce6fb73229169aa4e57ff4ae7dd79dfbd9
Pierre Bruat <pbruat33@gmail.com>
2022-02-01 14:16:57 +0100

    Update indexnow_streamlit.py

--- indexnow_streamlit.py
+++ indexnow_streamlit.py
@@ -11,8 +11,18 @@ import streamlit as st
 header = st.title('Index my urls now')
 form = st.form(key='my-form')
 api_key = form.text_input("Insert your API key")
-xml_sitemap = form.text_input("Insert your XML sitemap url")
 urls_list = form.file_uploader("Choose a CSV file", accept_multiple_files=True)
+if submit:
+	for i in urls_list:
+		endpoint= f"https://bing.com/indexnow?url={i}&key={api_key}"
+		response = requests.get(endpoint)
+		st.write(response)
+		if response != "error":
+			st.write(f"✅ URL submitted successfully for {i}")
+		else: st.write(f"❌ something went wrong with {i}")
+		time.sleep(2)
+
+xml_sitemap = form.text_input("Insert your XML sitemap url")
 submit = form.form_submit_button('Submit')
 if submit:
 	sitemap_urls = adv.sitemap_to_df(xml_sitemap)
