f862c2f0e2f56d2646884f8cd2d1869ad075a8e7
Pierre Bruat <pbruat33@gmail.com>
2022-02-01 14:31:14 +0100

    Update indexnow_streamlit.py

--- indexnow_streamlit.py
+++ indexnow_streamlit.py
@@ -10,11 +10,11 @@ import streamlit as st
 ###### FORMULAIRE ########
 header = st.title('Index my urls now')
 
-form_1 = st.form(key='my-form')
-api_key = form_1.text_input("Insert your API key")
-urls_list = form_1.file_uploader("Choose a CSV file", accept_multiple_files=True)
-submit_1 = form_1.form_submit_button('Submit')
-if submit_1:
+form = st.form(key='my-form')
+api_key = form.text_input("Insert your API key")
+urls_list = form.file_uploader("Choose a CSV file", accept_multiple_files=True)
+submit = form.form_submit_button('Submit')
+if submit:
 	for i in urls_list:
 		endpoint= f"https://bing.com/indexnow?url={i}&key={api_key}"
 		response = requests.get(endpoint)
@@ -24,11 +24,9 @@ if submit_1:
 		else: st.write(f"❌ something went wrong with {i}")
 		time.sleep(2)
 
-form_2 = st.form(key='my-form')
-api_key = form_2.text_input("Insert your API key")
-xml_sitemap = form_2.text_input("Insert your XML sitemap url")
-submit_2 = form_2.form_submit_button('Submit')
-if submit_2:
+xml_sitemap = form_.text_input("Insert your XML sitemap url")
+submit = form.form_submit_button('Submit')
+if submit:
 	sitemap_urls = adv.sitemap_to_df(xml_sitemap)
 	urls = sitemap_urls["loc"].to_list()
 	for i in urls:
