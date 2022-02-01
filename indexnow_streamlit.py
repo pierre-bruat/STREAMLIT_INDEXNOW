50bcf38e7a305f71e9c36a846e83313659da4b99
Pierre Bruat <pbruat33@gmail.com>
2022-02-01 17:58:24 +0100

    Update indexnow_streamlit.py

--- indexnow_streamlit.py
+++ indexnow_streamlit.py
@@ -21,6 +21,7 @@ submit = form.form_submit_button('Submit')
 if submit:
 	sitemap_urls = adv.sitemap_to_df(xml_sitemap)
 	urls = sitemap_urls["loc"].to_list()
+	urls_list = urls_list.split(",")
 	for i in urls, urls_list:
 		endpoint= f"https://bing.com/indexnow?url={i}&key={api_key}"
 		response = requests.get(endpoint)
