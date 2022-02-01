7f82e8ef8f3e212a0b5617a60551dc0f08a1fe8d
Pierre Bruat <pbruat33@gmail.com>
2022-02-01 18:46:15 +0100

    Update indexnow_streamlit.py

--- indexnow_streamlit.py
+++ indexnow_streamlit.py
@@ -17,50 +17,40 @@ def input_to_df(input):
 	return df
 
 
+st.expander('Index my URLS now with list of urls')
+	form = st.form(key='my-form')
+	api_key = form.text_input("Insert your API key")
+	uploaded_file = form.file_uploader("Upload your CSV file")
+	submit = form.form_submit_button('Submit')
+	if submit:
+		#sitemap_urls = adv.sitemap_to_df(xml_sitemap)
+		#sitemap_urls = sitemap_urls["loc"].to_list()
+		urls_loaded = input_to_df(uploaded_file)
+		urls_loaded = urls_loaded["urls"].to_list()
+		for i in urls_loaded:
+			endpoint= f"https://bing.com/indexnow?url={i}&key={api_key}"
+			response = requests.get(endpoint)
+			if response != "error":
+				st.write(f"✅ URL submitted successfully for {i}")
+			else: st.write(f"❌ something went wrong with {i}")
+			time.sleep(2)
 
-header = st.title('Index my urls now with XML sitemap')
-st.expander('STEP 1: Configure your extraction')
-form = st.form(key='my-form')
-api_key = form.text_input("Insert your API key")
-uploaded_file = form.file_uploader("Upload your CSV file")
-submit = form.form_submit_button('Submit')
-if submit:
-	#sitemap_urls = adv.sitemap_to_df(xml_sitemap)
-	#sitemap_urls = sitemap_urls["loc"].to_list()
-	urls_loaded = input_to_df(uploaded_file)
-	urls_loaded = urls_loaded["urls"].to_list()
-	for i in urls_loaded:
-		endpoint= f"https://bing.com/indexnow?url={i}&key={api_key}"
-		response = requests.get(endpoint)
-		if response != "error":
-			st.write(f"✅ URL submitted successfully for {i}")
-		else: st.write(f"❌ something went wrong with {i}")
-		time.sleep(2)
 
-	#for y in sitemap_urls:
-		#endpoint= f"https://bing.com/indexnow?url={y}&key={api_key}"
-		#response = requests.get(endpoint)
-		#if response != "error":
-			#st.write(f"✅ URL submitted successfully for {y}")
-		#else: st.write(f"❌ something went wrong with {y}")
-		#time.sleep(2)
-
-st.expander('STEP 1: Configure your extraction')
-header_2 = st.title('Index my urls now with list of urls')
-form_2 = st.form(key='my-form-2')
-api_key = form_2.text_input("Insert your API key")
-xml_sitemap = form_2.text_input("Insert your XML sitemap url")
-submit_2 = form_2.form_submit_button('Submit')
-if submit_2:
-	sitemap_urls = adv.sitemap_to_df(xml_sitemap)
-	sitemap_urls = sitemap_urls["loc"].to_list()
-	#urls_loaded = input_to_df(uploaded_file)
-	#urls_loaded = urls_loaded["urls"].to_list()
-	for y in sitemap_urls:
-		endpoint= f"https://bing.com/indexnow?url={y}&key={api_key}"
-		response = requests.get(endpoint)
-		if response != "error":
-			st.write(f"✅ URL submitted successfully for {y}")
-		else: st.write(f"❌ something went wrong with {y}")
-		time.sleep(2)
+with st.expander('Index my URLS now with XML sitemap'):
+	form_2 = st.form(key='my-form-2')
+	api_key = form_2.text_input("Insert your API key")
+	xml_sitemap = form_2.text_input("Insert your XML sitemap url")
+	submit_2 = form_2.form_submit_button('Submit')
+	if submit_2:
+		sitemap_urls = adv.sitemap_to_df(xml_sitemap)
+		sitemap_urls = sitemap_urls["loc"].to_list()
+		#urls_loaded = input_to_df(uploaded_file)
+		#urls_loaded = urls_loaded["urls"].to_list()
+		for y in sitemap_urls:
+			endpoint= f"https://bing.com/indexnow?url={y}&key={api_key}"
+			response = requests.get(endpoint)
+			if response != "error":
+				st.write(f"✅ URL submitted successfully for {y}")
+			else: st.write(f"❌ something went wrong with {y}")
+			time.sleep(2)
 
